d070b835ffa1c4efb9d0909adbbece20a30a38de
Harshpratap-singh <107066198+Harshpratap-singh@users.noreply.github.com>
2026-01-01 21:54:06 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = new express();
+app.use("/",(req,res)=>{
+    res.send("hello from the sever")
+});
+app.use("/test",(req,res)=>{
+    res.send("hello from the sever test")
+});
+app.use("/home",(req,res)=>{
+    res.send("hello from the sever home")
+});
+
+app.listen(3000, () => {
+  console.log("we are now starting");
+});
